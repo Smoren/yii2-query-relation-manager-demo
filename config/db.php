bfb67c110c6a4894c95484737dde5bf852cfb703
Smoren <ofigate@gmail.com>
2020-01-03 18:41:44 +0300

    upd

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=test',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_query_relation_manager_demo',
     'username' => 'user',
     'password' => '123456789',
     'charset' => 'utf8',
